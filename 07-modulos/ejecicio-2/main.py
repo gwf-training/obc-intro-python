13a1229e9bcc0bd6440fa1135a55a89806db33fa
gwfernandez <gwfernandez@gmail.com>
2022-11-11 22:01:59 -0300

    ejercicio 2. Alarma

---
+++ 07-modulos/ejecicio-2/main.py
@@ -0,0 +1,17 @@
+import time
+from datetime import datetime, timedelta
+
+def main():
+    print("Iniciando Alarma: Go home ...")
+    horaAlarmaGoHome = 23
+    horaActual = int(time.strftime("%H"))
+    if horaActual >= horaAlarmaGoHome:
+        print("Son las ", time.strftime("%H:%M:%S"),"Ya es hora de ir a casa!!")
+    else:
+        alarmaGoHome = datetime.strptime(time.strftime("%Y-%m-%d")+" "+str(horaAlarmaGoHome)+":00:00", "%Y-%m-%d %H:%M:%S")
+        print("alarma %s:" % alarmaGoHome)
+        diff = alarmaGoHome - datetime.now()
+        print("Vamos!!! solo faltan %s" % diff)
+
+if __name__ == "__main__":
+    main()
